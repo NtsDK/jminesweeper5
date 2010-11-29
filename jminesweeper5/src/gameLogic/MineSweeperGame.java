6d1d01a4283071bfc8692f0cd1023bcd94973f19
trechkalov <trechkalov@801ed718-bf6f-2f44-3cbc-b64a8f0bab3b>
2010-11-29 14:27:10 +0000

    Implemented cell neighbours finding algo. Extend setMines algo. Refactor tests.

--- jminesweeper5/src/gameLogic/MineSweeperGame.java
+++ jminesweeper5/src/gameLogic/MineSweeperGame.java
@@ -1,7 +1,9 @@
 package gameLogic;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MineSweeperGame {
 
@@ -9,6 +11,14 @@ public class MineSweeperGame {
   private final int xSize = 20, ySize = 10;
   private boolean isGameEnded = false;
   private static Map<CellState, String> cellState2String = new HashMap<CellState, String>();
+  
+  public int getXSize() {
+    return xSize;
+  }
+  
+  public int getYSize() {
+    return ySize;
+  }
   
   static {
     cellState2String.put(CellState.OPEN, "O");
@@ -24,6 +34,62 @@ public class MineSweeperGame {
     }
   }
   
+  public void setMines(int minesNumber) {
+    Random random = new Random();
+    while(minesNumber>0) {
+      int x = random.nextInt(xSize);
+      int y = random.nextInt(ySize);
+      Cell cell = gameField.get(new CellCoords(x, y));
+      if(cell.getCellType()!=CellType.MINE) {
+        cell.setMine();
+        minesNumber--;
+      }
+    }
+    for(int y=0; y<ySize; ++y) {
+      for(int x=0; x<xSize; ++x) {
+        Cell cell = gameField.get(new CellCoords(x, y));
+        if(cell.getCellType()!=CellType.MINE) {
+          try {
+            ArrayList<CellCoords> neighboursCoords = getNeighboursCoords(x, y);
+            int neighbourMineNumber = 0;
+            for(CellCoords cellCoords:neighboursCoords) {
+              if(gameField.get(cellCoords).getCellType()==CellType.MINE ) {
+                neighbourMineNumber++;
+              }
+            }
+            cell.setCellValue(neighbourMineNumber);
+          } catch (OutOfFieldException e) {
+            e.printStackTrace();
+          }
+          
+        }
+        
+        //gameField.put(new CellCoords(x, y), new Cell(new CellCoords(x, y)));
+      }
+    }
+  }
+  
+  public class OutOfFieldException extends Exception {
+    
+  }
+  
+  public ArrayList<CellCoords> getNeighboursCoords(int x, int y) throws OutOfFieldException {
+    // TODO Auto-generated method stub
+    ArrayList<CellCoords> neighboursCoords = new ArrayList<CellCoords>();
+    if(x<0 || y<0 || x>=xSize || y>=ySize) {
+      throw new OutOfFieldException();
+    }
+    if(x>0       && y>0      ) neighboursCoords.add(new CellCoords(x-1, y-1));
+    if(x>0                   ) neighboursCoords.add(new CellCoords(x-1,  y ));
+    if(x>0       && y<ySize-1) neighboursCoords.add(new CellCoords(x-1, y+1));
+    if(             y<ySize-1) neighboursCoords.add(new CellCoords( x , y+1));
+    if(x<xSize-1 && y<ySize-1) neighboursCoords.add(new CellCoords(x+1, y+1));
+    if(x<xSize-1             ) neighboursCoords.add(new CellCoords(x+1,  y ));
+    if(x<xSize-1 && y>0      ) neighboursCoords.add(new CellCoords(x+1, y-1));
+    if(             y>0      ) neighboursCoords.add(new CellCoords( x , y-1));
+    
+    return neighboursCoords;
+  }
 
   public Cell getCell(CellCoords cellCoords) {
     return gameField.get(cellCoords);
--- jminesweeper5/src/tests/MineSweeperGameTest.java
+++ jminesweeper5/src/tests/MineSweeperGameTest.java
@@ -1,50 +1,109 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+
 import gameLogic.Cell;
 import gameLogic.CellCoords;
 import gameLogic.MineSweeperGame;
+import gameLogic.MineSweeperGame.OutOfFieldException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
-public class MineSweeperGameTest {
+public class MineSweeperGameTest{
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+  private MineSweeperGame mineSweeperGame;
 
+  @Before
+  public void initialize(){
+    mineSweeperGame = new MineSweeperGame();
   }
   
   @Test
   public void testOpenCell() {
-    MineSweeperGame mineSweeperGame = new MineSweeperGame();
     assertEquals(mineSweeperGame.openCell(0, 0),8);
   }
   
   @Test
   public void testGetCell() {
-    MineSweeperGame mineSweeperGame = new MineSweeperGame();
     assertEquals(mineSweeperGame.getCell(new CellCoords(0, 0)),new Cell(new CellCoords(0, 0)));
   }
   
   @Test
   public void testMarkCell() {
-    MineSweeperGame mineSweeperGame = new MineSweeperGame();
     assertEquals(mineSweeperGame.markCell(0, 0),6);
   }
   
   @Test
   public void testPrintField() {
-    MineSweeperGame mineSweeperGame = new MineSweeperGame();
     mineSweeperGame.getCell(new CellCoords(5, 5)).flagCell();
     mineSweeperGame.getCell(new CellCoords(4, 5)).openCell();
     mineSweeperGame.getCell(new CellCoords(8, 5)).openCell();
     mineSweeperGame.getCell(new CellCoords(8, 5)).openCell();
     mineSweeperGame.printField();
+  }
+  
+  @Test
+  public void testPrintFieldContents() {
+    mineSweeperGame.setMines(10);
+    mineSweeperGame.getCell(new CellCoords(5, 5)).setCellValue(3);
+    mineSweeperGame.getCell(new CellCoords(5, 6)).setCellValue(5);
     mineSweeperGame.printFieldContents();
+  }
+ 
+  @Test
+  public void testGetNeighboursCoords() throws OutOfFieldException {
+    int xSize = mineSweeperGame.getXSize();
+    int ySize = mineSweeperGame.getYSize();
+    
+    ArrayList<CellCoords> neighboursCoords = mineSweeperGame.getNeighboursCoords(0,0);
+    ArrayList<CellCoords> neighboursCoordsCorrectValue = new ArrayList<CellCoords>();
+    neighboursCoordsCorrectValue.add(new CellCoords(0, 1));
+    neighboursCoordsCorrectValue.add(new CellCoords(1, 1));
+    neighboursCoordsCorrectValue.add(new CellCoords(1, 0));
+    assertEquals(neighboursCoordsCorrectValue, neighboursCoords);
+    
+    neighboursCoords = mineSweeperGame.getNeighboursCoords(xSize-1,0);
+    neighboursCoordsCorrectValue.clear();
+    neighboursCoordsCorrectValue.add(new CellCoords(xSize-2, 0));
+    neighboursCoordsCorrectValue.add(new CellCoords(xSize-2, 1));
+    neighboursCoordsCorrectValue.add(new CellCoords(xSize-1, 1));
+    assertEquals(neighboursCoordsCorrectValue, neighboursCoords);
+    
+    neighboursCoords = mineSweeperGame.getNeighboursCoords(xSize-1,ySize-1);
+    neighboursCoordsCorrectValue.clear();
+    neighboursCoordsCorrectValue.add(new CellCoords(xSize-2, ySize-2));
+    neighboursCoordsCorrectValue.add(new CellCoords(xSize-2, ySize-1));
+    neighboursCoordsCorrectValue.add(new CellCoords(xSize-1, ySize-2));
+    assertEquals(neighboursCoordsCorrectValue, neighboursCoords);
+    
+    neighboursCoords = mineSweeperGame.getNeighboursCoords(0,ySize-1);
+    neighboursCoordsCorrectValue.clear();
+    neighboursCoordsCorrectValue.add(new CellCoords(1, ySize-1));
+    neighboursCoordsCorrectValue.add(new CellCoords(1, ySize-2));
+    neighboursCoordsCorrectValue.add(new CellCoords(0, ySize-2));
+    assertEquals(neighboursCoordsCorrectValue, neighboursCoords);
+    
+    int centerX = xSize/2;
+    int centerY = ySize/2;
+    neighboursCoords = mineSweeperGame.getNeighboursCoords(centerX,centerY);
+    neighboursCoordsCorrectValue.clear();
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX-1, centerY-1));
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX-1, centerY));
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX-1, centerY+1));
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX, centerY+1));
+    
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX+1, centerY+1));
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX+1, centerY));
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX+1, centerY-1));
+    neighboursCoordsCorrectValue.add(new CellCoords(centerX, centerY-1));
+
+    assertEquals(neighboursCoordsCorrectValue, neighboursCoords);
   }
 
 }
